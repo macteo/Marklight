71c58cd6d592630862bda262cc95a886ddb870db
Christian Tietze <christian.tietze@gmail.com>
2017-07-19 10:51:56 +0200

    add macOS sample project

---
+++ Sample/Marklight Sample macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Marklight Sample macOS
+//
+//  Created by Christian Tietze on 19.07.17.
+//  Copyright © 2017 Dolomate. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sample/Pods/Target Support Files/Pods-Marklight-Marklight Sample/Pods-Marklight-Marklight Sample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Marklight_Marklight_Sample : NSObject
+@end
+@implementation PodsDummy_Pods_Marklight_Marklight_Sample
+@end
